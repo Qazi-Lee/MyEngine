8b97fdaa1c3c34f16bec6a168f22cd8187794598
Qazi-Lee <160686515+Qazi-Lee@users.noreply.github.com>
2025-04-15 19:23:44 +0800

    清理文件夹

--- test/DerivedClass.cpp
+++
@@ -1,5 +0,0 @@
-#include"BaseClass.h"
-// DerivedClass.h
-
-
-
--- test/miniaudio.c
+++
@@ -1,5 +0,0 @@
-#define STB_VORBIS_HEADER_ONLY
-#include "stb_vorbis.c"    // 包含 Vorbis 解码实现
-#define MA_ENABLE_VORBIS   // 告诉 miniaudio 启用 Vorbis 支持
-#define MINIAUDIO_IMPLEMENTATION
-#include "miniaudio.h"
